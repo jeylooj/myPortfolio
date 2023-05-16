2b5deabd99f8d41fbf51b60429a9a3f02e0ba492
Jeyran <jeylooj@gmail.com>
2023-05-16 10:52:00 +0200

    improve texts and put fav icon

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
         <Image src='/Images/Jeyran.png' width={160} height={160} alt='Jeyran Jaberi' className='border-blue border-4 rounded-[50%]'/>
         <h3 className='bebas-font text-[56px] bg-lightPink'>Front-End, <br /> React Developer </h3>
       </div>
-      <p className='py-4 bg-lightPink'>"As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques."</p>
+      <p className='py-4 bg-lightPink'>"As a Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Next Js, Tailwind, and SCSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques."</p>
       <p className='py-1 bg-lightPink '>
       "I enjoy a variety of activities in my free time, including watching anime, solving puzzles, drawing and exploring different types of food."
       </p>
--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,11 @@ const inria = Inria_Sans({weight:['300','400','700'], subsets: ['latin'] })
 export const metadata = {
   title: 'JEYLOOJ',
   description: 'FrontEnd developer Portfolio',
+  icons: [
+    { "src": "../favicon.ico", "type": "image/png", "sizes": "192x192" },
+    { "src": "../favicon.ico", "type": "image/png", "sizes": "512x512" }
+  ]
+  
 }
 
 export default function RootLayout({ children }) {
